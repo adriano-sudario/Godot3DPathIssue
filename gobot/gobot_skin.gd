4837d0d500c7482702ab1dd946e37c19dadce263
Adriano Sudário <sudario.adriano@gmail.com>
2023-09-05 17:14:13 -0300

    .

--- gobot/gobot_skin.gd
+++ gobot/gobot_skin.gd
@@ -9,11 +9,11 @@ signal foot_step
 @onready var path_follow_3d: PathFollow3D = $".."
 
 func _ready():
+	_state_machine.travel("Run")
 	run()
 
 func run():
-	_state_machine.travel("Run")
 	path_follow_3d.progress_ratio = 0
-	var tween = get_tree().create_tween().set_loops()
+	var tween = get_tree().create_tween()
 	tween.tween_property(path_follow_3d, "progress_ratio", 1, 3)
 	tween.tween_callback(run)
